66c22a830e620a2c71a09f166d26d24b0705831b
thunderB <thunderB@protonmail.com>
2019-04-10 20:22:32 -0400

    works for bitcoin

--- kraken-price/src/main.rs
+++ kraken-price/src/main.rs
@@ -3,18 +3,48 @@ use requests;
 use requests::ToJson;
 
 fn main() {
+    // collect command line argument if given
     let args: Vec<String> = env::args().collect();
 
-    let coin = &args[1];
-    println!("the coin requested is {}", coin);
+    // if argument was given put it into variable arg
+    let arg1 = &args[1];
 
-    let url = "https://api.kraken.com/0/public/Ticker?pair=XBTUSD";
-    let response = requests::get(url).unwrap();
+    // execute query on bitcoin and doge, Rick Roll anything else
+    match arg1.as_str() {
+        "btc" => {
+            let price = query_api(arg1);
+            println!("{}", price);
+        },
+        _ => println!("something else")
+    }
+}
+
+
+
+
+fn query_api(coin: &String) -> String {
+    // let baseURL = "https://api.kraken.com/0/public/Ticker?pair=";
+    // let url = "https://api.kraken.com/0/public/Ticker?pair=XBTUSD";
+
+    // let response;
+    let price;
+
+    match coin.as_str() {
+        "btc" => {
+            let url = concat!("https://api.kraken.com/0/public/Ticker?pair=", "XBTUSD");
+            let response = requests::get(url).unwrap();
+            let full_data = response.json().unwrap();
+            price = full_data["result"]["XXBTZUSD"]["c"][0].to_string();
+        },
+        _ => {
+            price = String::from("not a valid coin");
+            println!("not a coin");
+        }
+    }
+
 
 
     // println!("{}", response.status_code());
-
-
-    let data = response.json().unwrap();
-    println!("$ {}", data["result"]["XXBTZUSD"]["c"][0]);
+    // println!("$ {}", price);
+    price
 }
